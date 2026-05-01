d1629cff69c890cf492ea7ed8d47936616373ae5
Dano <polludomaster@gmail.com>
2026-04-30 18:29:52 -0600

    refactor: rename Dictionary internal storage to avoid name duplication

--- app/dictionary/dictionary.py
+++ app/dictionary/dictionary.py
@@ -3,12 +3,12 @@ from typing import Dict
 
 class Dictionary:
     def __init__(self) -> None:
-        self.dictionary: Dict[str, str] = {}
+        self.entries: Dict[str, str] = {}
 
     def newentry(self, word: str, definition: str) -> None:
-        self.dictionary[word] = definition
+        self.entries[word] = definition
 
     def look(self, word: str) -> str:
-        if word in self.dictionary:
-            return self.dictionary[word]
+        if word in self.entries:
+            return self.entries[word]
         return f"Can't find entry for {word}"
