4e06712cc8a0304d9eb0c1ea47dd284e63306b3b
Dano <polludomaster@gmail.com>
2026-05-05 17:26:14 -0600

    ci: EC2 automated deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ resource "aws_instance" "ci_cd_instance" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
 
   user_data = file("${path.module}/user_data.sh")
 
